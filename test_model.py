6e4d49b9a5b6a0707ed67149db761c59f63a6ba2
IIDA Yusuke <iibasecond@gmail.com>
2025-01-02 13:23:08 +0900

    テストデータのディレクトリ入力を追加し、モデルのハイパーパラメータを調整。データローダーを使用してテストを実行し、損失を計算する機能を実装。

--- test_model.py
+++ test_model.py
@@ -2,35 +2,56 @@
 import torch
 from load_data import load_data
 from model import SDRegressionModel
+from torch_geometric.loader import DataLoader
 
+# CUDAの設定
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-input = input('base_dir: ')
+test_data_dir = input('test_data_dir: ')
+model_dir = input('model_dir: ')
 
-pkl_file = f'{input}/test.pkl'
-model_file = f'{input}/model.pth'
+pkl_file = f'{test_data_dir}/test.pkl'
+model_file = f'{model_dir}/model.pth'
 
 # データの読み込み
 test_data_set = load_data(pkl_file)
 
-test_data_set = test_data_set[1000]
 
-# --- モデルの定義 ---
-hidden_channels = 108
-SAGE_num_layers = 5
-connection_channels = 90
-dropout = 0.15
-lr = 0.00034
-batch_size = 64
-fc_num_layers = 3
+# --- ハイパーパラメータ ---
+hidden_channels = 121
+connection_channels = 110
+SAGE_num_layers = 9
+fc_num_layers = 5
+lr = 0.00041098201353148003
+batch_size = 16
 
 model = SDRegressionModel(in_channels=4, hidden_channels=hidden_channels, SAGE_num_layers=SAGE_num_layers, connection_channels=connection_channels, fc_num_layers=fc_num_layers)
 model.load_state_dict(torch.load(model_file))
 
-# --- テスト ---
+test_data_loader = DataLoader(test_data_set, batch_size=1024, shuffle=False)
 
+# --- テスト ---
 model.eval()
 with torch.no_grad():
-    data = test_data_set
-    out = model(data.x, data.edge_index, data.sd_index)
-    for i in range(len(out)):
-        print(f'{data.y[i]} {out[i]} {round(out[i].item()) == data.y[i]}')
+    total_loss = 0
+    for data in test_data_loader:
+        out = model(data.x, data.edge_index, data.sd_index)
+        loss = torch.nn.MSELoss()(out, data.y)
+        total_loss += loss.item()
+    print(f'Loss: {total_loss / len(test_data_loader)}')
+
+
+# --- テスト ---
+
+# model.eval()
+# with torch.no_grad():
+#     while True:
+#         print('index: ')
+#         index = int(input())
+#         data = test_data_set[index]
+#         out = model(data.x, data.edge_index, data.sd_index)
+#         for i in range(len(out)):
+#             print(f'{data.y[i]} {out[i]} {round(out[i].item()) == data.y[i]}')
+        
+#         accuracy = sum([round(out[i].item()) == data.y[i] for i in range(len(out))]) / len(out)
+#         print(f'accuracy: {accuracy}')
